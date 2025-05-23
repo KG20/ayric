aa35458940053cbf657bfa22850728c583ea81f9
KG20 <noo.limitss@gmail.com>
2025-05-23 19:36:37 +0530

    commented code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,9 +3,9 @@
 session_start();
 
 // Load environment variables
-require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+// require __DIR__ . '/../vendor/autoload.php';
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+// $dotenv->load();
 
 // Load configuration
 $config = require __DIR__ . '/../config/app.php';
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 // public/index.php
 require __DIR__ . '/../bootstrap/app.php';
-$router = new App\Core\Router();
+$router = new app\Core\Router();
 require __DIR__ . '/../routes/web.php';
 $router->dispatch();
